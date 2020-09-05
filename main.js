9ca171ab3d1fb63a3c53f03b8ba9cb3ea277551a
Ricardo Arana Reyes <raranare@everis.com>
2020-09-05 18:13:26 -0500

    POO

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 "use strict";
 (function () {
+    class Carro {
+        constructor(color, fabricante, modelo, fechaFabricacion) {
+            this.color = color;
+            this.fabricante = fabricante;
+            this.modelo = modelo;
+            this.fechaFabricacion = fechaFabricacion;
+        }
+        obtenerColor() {
+            return this.color;
+        }
+    }
+    let auto = new Carro('Azul', 'Nissan', 'Delorian', new Date());
+    console.log(auto.obtenerColor());
+    console.log(auto.modelo);
 })();
--- main.ts
+++ main.ts
@@ -1,5 +1,28 @@
 (function() {
 
+    interface Icarro {
+        fechaFabricacion: Date; 
+    }
+
+    class Carro implements Icarro{
+        fechaFabricacion: Date;
+        constructor(private color: string,
+                    private fabricante: string,
+                    public modelo: string,
+                    fechaFabricacion: Date) {
+                        this.fechaFabricacion = fechaFabricacion;
+        }
+       
+
+        obtenerColor() {
+            return this.color;
+        }
+    }
+
+    let auto = new Carro('Azul', 'Nissan', 'Delorian', new Date());
+    console.log(auto.obtenerColor());
+    console.log(auto.modelo);
+
 })();
 
 
