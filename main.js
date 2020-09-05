f4e2428f1791cd6790f4350d9f40b5396a9b8a1f
Ricardo Arana Reyes <raranare@everis.com>
2020-09-05 16:28:56 -0500

    tipos

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 "use strict";
 (function () {
-    var persona = 'Carlos';
-    var persona2 = 'Claudio';
-    var URL = 'https://everis.com';
-    if (true) {
-        var URL_1 = '';
-        persona = 'Pedro';
-        console.log('nombre: ' + persona);
+    var nuevaVariable;
+    var nombre;
+    nombre = 'Ricardo';
+    var edad;
+    edad = 29;
+    var esDev;
+    esDev = true; // true false edad > 30
+    var fecha = new Date(); // 2020/11/12
+    var direccion = { calle: 'Portocarrero', num: '262' };
+    direccion = 'Portocarrero';
+    direccion = 262;
+    direccion = true;
+    var name;
+    name = 'Ricardo Arana';
+    name = 234;
+    var asistencia = ['Ricardo',
+        'Victor',
+        'Jannet',
+        'Laleshca'];
+    function sumar(a, b) {
+        return a + b + 3;
     }
-    console.log('nombre: ' + persona);
 })();
